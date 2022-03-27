97f74b4b32247c01c487a8884939f31bcb879108
lodenrogue <phyzix5761@gmail.com>
2022-03-27 19:21:29 -0400

    Added tests for ensuring other sown or harvested crops are not removed when removing a crop

--- src/test/java/com/arkvis/garden/RemoveCropTest.java
+++ src/test/java/com/arkvis/garden/RemoveCropTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class RemoveCropTest {
@@ -29,6 +30,50 @@ class RemoveCropTest {
         bed.remove(crop);
         List<Crop> history = bed.getHistory(0);
         assertTrue(history.isEmpty());
+    }
+
+    @Test
+    void should_notRemoveOtherSownCrops_when_removingSownCrop() {
+        Crop basil = new Crop("basil", "mint", 36, 4);
+        Crop corn = new Crop("corn", "grass", 36, 4);
+        Crop carrot = new Crop("carrot", "parsley", 36, 1);
+
+        Bed bed = new Bed(2);
+        bed.sow(0, basil);
+        bed.sow(0, corn);
+        bed.sow(1, carrot);
+
+        bed.remove(basil);
+
+        assertEquals(1, bed.getSownCrops(0).size());
+        assertTrue(bed.getSownCrops(0).contains(corn));
+
+        assertEquals(1, bed.getSownCrops(1).size());
+        assertTrue(bed.getSownCrops(1).contains(carrot));
+    }
+
+    @Test
+    void should_notRemoveOtherCropsFromHistory_when_removingHarvestedCrop() {
+        Crop basil = new Crop("basil", "mint", 36, 4);
+        Crop corn = new Crop("corn", "grass", 36, 4);
+        Crop carrot = new Crop("carrot", "parsley", 36, 1);
+
+        Bed bed = new Bed(2);
+        bed.sow(0, basil);
+        bed.sow(0, corn);
+        bed.sow(1, carrot);
+
+        bed.harvest(basil);
+        bed.harvest(corn);
+        bed.harvest(carrot);
+
+        bed.remove(basil);
+
+        assertEquals(1, bed.getHistory(0).size());
+        assertTrue(bed.getHistory(0).contains(corn));
+
+        assertEquals(1, bed.getHistory(1).size());
+        assertTrue(bed.getHistory(1).contains(carrot));
     }
 
     @Test
